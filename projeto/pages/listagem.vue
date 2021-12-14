1f5eea37e5c35dd04a25240ef9bc8f05de2519fe
emotton <emotton@gmail.com>
2021-12-13 23:18:05 -0300

    Inclusão projeto

---
+++ projeto/pages/listagem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Teste</h1>
+    <h5>{{ nome }}</h5>
+    <ul>
+      <li v-for="prod in produtos" :key="prod.id">
+        {{ prod.nome }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListagemPage",
+  data() {
+    return {
+      nome: "Eduardo Motton",
+      produtos: [
+        { id: 1, nome: "geladeira" },
+        { id: 2, nome: "televisão" },
+        { id: 3, nome: "cadeira" },
+      ],
+    };
+  },
+};
+</script>
