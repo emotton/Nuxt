b2130d207cead092e32cc74c97432b463d067d59
Eduardo Motton <emotton@gmail.com>
2022-01-31 22:55:47 -0300

    Atualizacao

--- testes/nuxt.config.js
+++ testes/nuxt.config.js
@@ -3,10 +3,10 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - projeto',
-    title: 'projeto',
+    titleTemplate: '%s - testes',
+    title: 'testes',
     htmlAttrs: {
-      lang: 'pt-br'
+      lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
@@ -25,7 +25,6 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '@/plugins/my-plugin'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -34,19 +33,19 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify'
+    '@nuxtjs/vuetify',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/'
+    baseURL: '/',
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
@@ -66,10 +65,6 @@ export default {
         }
       }
     }
-  },
-
-  router: {
-    middleware: 'test'
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
